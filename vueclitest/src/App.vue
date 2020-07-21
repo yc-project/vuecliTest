298684e003096c9c50853ee1733ad6e200097c39
yc-project <208768637@qq.com>
2020-07-21 10:40:37 +0800

    初始化

---
+++ vueclitest/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+  
+    <login msg="欢迎来到刘一手火锅商城登录页面"/>
+  </div>
+</template>
+
+<script>
+import login from './components/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+   login 
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vueclitest/src/components/login.vue
@@ -0,0 +1,52 @@
+<template>
+   <div id="loginApp" class="loginApp">
+    <div style="width:100%;height:60px;background-color:#800000;">
+      <div style="height:10px"></div>
+      <font style="margin-left:700px;font-weight:bolder;font-size:25px;color:white">登录</font>
+    </div>
+   
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data:function(){
+    return{ 
+      username: "",
+      userpwd: ""
+      }
+  },
+  methods:{
+
+    login() {
+          if (this.username == "yc" && this.userpwd == "123") {
+            window.alert("登录成功！是否确认跳转？")
+           // window.location.href="MenuFirst.html"
+          } else {
+            window.alert("登录失败")
+           
+          }
+        },
+         register(){
+           console.log("注册")
+          //window.location.href="Regist.html"
+         }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+ .loginApp{
+  
+   width:100%;
+   height: 100%;
+ }
+
+</style>
